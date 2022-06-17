2477d9f954445c754b25c20db2d176b1de58b785
antoncarton <aksalutly@gmail.com>
2022-06-17 11:27:38 +0500

    initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,9 @@
+public class BmiService {
+
+    public double calculate (double weight , double height){
+        double index;
+        index = weight / (height * height);
+        return index;
+
+    }
+}
